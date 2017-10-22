d9f2bc01b235fc565040dad7f71939afac2d4d2e
Dimitry Lyomin <leminds@gmail.com>
2017-10-22 16:48:55 +0300

    Add express server

---
+++ server/index.js
@@ -0,0 +1,11 @@
+var express = require('express');
+
+var app = express();
+
+app.use(express.static(__dirname + './../dist'));
+
+app.get('/', function (req, res) {
+  res.send(JSON.stringify({}));
+})
+
+app.listen(3000);
